469766f5b6dc43a00bd671852a16f4bbb2928a69
srujan-0709 <2025cs_bsrujan_b@nie.ac.in>
2026-09-22 15:19:37 +0530

    Pydentic Model

--- Day/Day2/FastAPIPractics/main.py
+++ Day/Day2/FastAPIPractics/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
@@ -21,3 +22,18 @@ def create_something():
 @app.get("/student/{usn}")
 def get_result(usn):
     return {"Result":"Distinction","usn":usn}
+
+# path parameter with Type Hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno: int):
+    return {"Result":"Distinction","rollno":rollno, "type": str(type(rollno))}
+
+# pydentic Model
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+@app.post("/items")
+def create_item(item: Item):
+    return {"received': item, 'total_price": item.price * 1.18} 
