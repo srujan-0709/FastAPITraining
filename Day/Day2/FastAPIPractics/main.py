0714bc4a942dfdad4cb6d1b125858f4c89152de6
srujan-0709 <2025cs_bsrujan_b@nie.ac.in>
2026-09-22 13:20:35 +0530

    path parameter

--- Day/Day2/FastAPIPractics/main.py
+++ Day/Day2/FastAPIPractics/main.py
@@ -11,3 +11,13 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+# post request
+@app.post("/create")
+def create_something():
+   return {"message": "created"}
+
+# path parameter
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
